e05e8d8043c27f8f6023a0c8e5d6a4b8964feb28
Alexander Milosh <alexander.milosh@softeq.com>
2021-03-15 22:28:28 +0300

    EDU-2 Create Spring Boot application.

---
+++ edu-amilosh/src/main/java/com/softeq/amilosh/EduApplication.java
@@ -0,0 +1,25 @@
+/*
+ * Developed by Softeq Development Corporation
+ * http://www.softeq.com
+ */
+
+package com.softeq.amilosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot application main class.
+ * <p/>
+ * Created on 2021-03-15
+ * <p/>
+ *
+ * @author Alexander Milosh
+ */
+@SpringBootApplication
+public class EduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class, args);
+    }
+}
